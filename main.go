4b7ba65616662070feb2ac4cafe4ecd43267941f
davidhenrygao <davidhenrygao@gmail.com>
2017-04-27 14:58:12 +0800

    finish code

--- main.go
+++ main.go
@@ -7,12 +7,20 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"strings"
 )
 
 const maxFileSize = 1024 * 1024
 const githubpath string = "/github.com/davidhenrygao/goInstallBinariesTool/"
 
-func OpenAndCheckFile(file string) *os.File {
+func closeFile(f *os.File) {
+	err := f.Close()
+	if err != nil {
+		fmt.Printf("Close file error: %v.\n", err)
+	}
+}
+
+func openAndCheckFile(file string) *os.File {
 	f, err := os.Open(file)
 	if err != nil {
 		fmt.Printf("Open file %s error: %v.\n", file, err)
@@ -21,19 +29,19 @@ func OpenAndCheckFile(file string) *os.File {
 	fInfo, err := f.Stat()
 	if err != nil {
 		fmt.Printf("Read file info error: %v.\n", err)
-		f.Close()
+		closeFile(f)
 		return nil
 	}
 	if fInfo.Size() > maxFileSize {
 		fmt.Printf("File size exceeds %d bytes(It is %d bytes).\n",
 			maxFileSize, fInfo.Size())
-		f.Close()
+		closeFile(f)
 		return nil
 	}
 	return f
 }
 
-func FindPkgsInFile(f *os.File) [][]byte {
+func findPkgsInFile(f *os.File) [][]byte {
 	fContent, err := ioutil.ReadAll(f)
 	if err != nil {
 		fmt.Printf("Read file error: %v.\n", err)
@@ -55,11 +63,24 @@ func FindPkgsInFile(f *os.File) [][]byte {
 	return pkgs
 }
 
-/*
-func ExecCmd(cmdline string) error {
-
+func execCmd(cmdline string) error {
+	cmdStrs := strings.Fields(cmdline)
+	if len(cmdStrs) == 0 {
+		return nil
+	}
+	prog := cmdStrs[0]
+	args := cmdStrs[1:]
+	cmd := exec.Command(prog, args...)
+	var errBuf bytes.Buffer
+	cmd.Stderr = &errBuf
+	fmt.Printf("Run Cmd: %s\n", cmdline)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("%s error: \n%s\n", cmdline, errBuf.String())
+		return err
+	}
+	return nil
 }
-*/
 
 func main() {
 	var inFile string
@@ -71,50 +92,34 @@ func main() {
 	gopath := os.Getenv("GOPATH") + "/src"
 	inFile = gopath + githubpath + inFile
 
-	f := OpenAndCheckFile(inFile)
+	f := openAndCheckFile(inFile)
 	if f == nil {
 		return
 	}
-	defer f.Close()
+	defer closeFile(f)
 
-	pkgs := FindPkgsInFile(f)
+	pkgs := findPkgsInFile(f)
 	if pkgs == nil {
 		return
 	}
 
-	cmd := exec.Command("go", "get", "-u", "github.com/gpmgo/gopm")
-	var errBuf bytes.Buffer
-	cmd.Stderr = &errBuf
-	fmt.Printf("Running go get gopm...\n")
-	err := cmd.Run()
+	err := execCmd("go get -u github.com/gpmgo/gopm")
 	if err != nil {
-		fmt.Printf("Run cmd error: \n%s\n",
-			errBuf.String())
 		return
 	}
 
-	var errFlag bool = false
+	var errFlag = false
 	var pkgStr string
 	for _, pkg := range pkgs {
 		pkgStr = string(pkg[1 : len(pkg)-1])
-		cmd = exec.Command("gopm", "get", "-g", "-d", pkgStr)
-		cmd.Stderr = &errBuf
-		fmt.Printf("gopm get %s.\n", pkgStr)
-		err = cmd.Run()
+		err = execCmd("gopm get -g -d " + pkgStr)
 		if err != nil {
 			errFlag = true
-			fmt.Printf("gopm get %s error: \n%s\n",
-				pkgStr, errBuf.String())
 			continue
 		}
-		cmd = exec.Command("go", "install", pkgStr)
-		cmd.Stderr = &errBuf
-		fmt.Printf("go install %s.\n", pkgStr)
-		err = cmd.Run()
+		err = execCmd("go install " + pkgStr)
 		if err != nil {
 			errFlag = true
-			fmt.Printf("go install %s error: \n%s\n",
-				pkgStr, errBuf.String())
 			continue
 		}
 	}
